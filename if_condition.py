bfe69b0b24c2bcf549e3b115c5fa00c3f1e7722b
TonyCheng <tony.pig@gmail.com>
2016-02-16 14:31:31 +0800

    2nd commit

--- if_condition.py
+++ if_condition.py
@@ -4,4 +4,12 @@ if pwd == 'apple':
     print('Loggin on ...')
 else:
     print('Incorrect password')
-print('All done!')
+print('done first condition!')
+
+age = raw_input('How old are you? ')
+if age <= 2:
+    print " free"
+elif 2 < age < 13:
+    print " child fare"
+else:
+    print " adult fare"
