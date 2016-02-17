270a636268f3a82933ff31e5fb80c815f83ce938
TonyCheng <tony.pig@gmail.com>
2016-02-17 17:49:52 +0800

    commit

--- str_slice.py
+++ str_slice.py
@@ -2,10 +2,13 @@ __author__ = 'Tony'
 def get_ext(fname):
     '''Returns the extension of file fname
           '''
-    dot = fname.rfind('.')
+    dot = fname.rfind('a')
     if dot == -1:
         return ''
-    else
+    else:
         return fname[dot + 1:]
 
-if __name__ == __main__:
+if __name__ == '__main__':
+    aa = get_ext('testing.tx');
+    bb = '  12345678  '
+    print '\'' + bb.rstrip() + '\''
