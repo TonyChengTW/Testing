f533daa4af09a0b14e45b736d276692382fb96a1
TonyCheng <tony.pig@gmail.com>
2016-02-17 15:31:29 +0800

    1st commit

--- area.py
+++ area.py
@@ -1,8 +1,9 @@
 __author__ = 'Tony'
-import math
-def area(radius):
-    return math.pi * radius ** 2
 
-if __name__ == '__main__':
-    aa = area(3)
-    print (aa)
+def shop(where = 'store',
+   what = 'pasta',
+   howmuch = '10 pounds'):
+   print 'I want you to go to the', where
+   print 'and buy' , howmuch, 'of', what + '.'
+
+shop(what='pen')
