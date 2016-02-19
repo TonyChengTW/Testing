150f6d4adc70b25c0f3eb0734fd0d710b536b3f2
TonyCheng <tony.pig@gmail.com>
2016-02-19 15:09:15 +0800

    commit

--- List_Ana.py
+++ List_Ana.py
@@ -1,1 +1,11 @@
 __author__ = 'Tony'
+def eat_vowerls(s):
+    """
+    :param s: Remoes the vowels from s
+    :return: s
+    """
+    return ''.join([c for c in s if c.lower() not in 'a'])
+
+print [n * n for n in range(1, 11)]
+print [a for a in 'pizza']
+print eat_vowerls('apple')
