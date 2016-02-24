85db050784c426e2560f30cd7a72c7a38999c002
TonyCheng <tony.pig@gmail.com>
2016-02-24 18:08:35 +0800

    change xxxx

---
+++ class/person2.py
@@ -0,0 +1,23 @@
+__author__ = 'Tony'
+
+class Person2(object):
+    def __init__(self):
+        self.__name = 'Tony'
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, realname):
+        self.__name = realname
+
+p = Person2()
+print "default name :", p.name
+
+p.name = 'Emily'
+print "change name : ", p.name
+print dir(p)
+
+#==========
+print p._Person2__name
--- class/private_method.py
+++ class/private_method.py
@@ -15,13 +15,23 @@ class ClassA:
     def do_nothing(self):
         pass
 
-class Person:
+class Person(object):
     def __init__(self, name = 'Default Name', age = 30):
-        self.name = name
-        self.age = age
+        self._name = name
+        self._age = age
+
+    @property
+    def age(self):
+        print "enter into property"
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        if 0 < age <= 150:
+            self._age = age
 
     def __str__(self):
-        return "print by __str__ - Person('%s',%d)" % (self.name, self.age)
+        return "print by __str__ - Person('%s',%d)" % (self._name, self._age)
 
 a = ClassA()
 a.say_hello("Tony")
@@ -29,9 +39,12 @@ print a
 
 # --------------------
 b = Person()
-b.name = 'Emily'
+b._name = 'Emily'
 b.age = 65
-print b
+print "b.age = ", b.age
+
+b.age = 200
+print "b.age = (200)", b.age
 
 c = Person()
 print c
