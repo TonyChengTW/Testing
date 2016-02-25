65262ac6970859c376f2c2ed7eb2bbe62f891471
Tony Cheng <tony.pig@gmail.com>
2016-02-25 10:53:25 +0800

    change xxxx

---
+++ packages/HTTP/file2.py
@@ -0,0 +1,18 @@
+from os.path import basename
+import requests
+import os
+
+save_path = 'school_list.csv'
+url = 'https://raw.github.com/moskytw/learning-python-from-data-examples/master/sql/schools.csv'
+
+def save(url, path=None):
+
+    if not path:
+        path = basename(url)
+
+    with open(path, 'w') as f:
+        f.write(requests.get(url).content)
+
+print 'os.getcwd=', os.getcwd()
+print os.getcwd().split('/')
+print basename(os.getcwd())
