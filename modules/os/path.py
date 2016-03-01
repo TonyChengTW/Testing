df24e8ee6cc25dcad1e66871d465516e14b90e8d
Tony Cheng <tony.pig@gmail.com>
2016-03-01 23:15:07 +0800

    commit xxxx

--- modules/os/path.py
+++ modules/os/path.py
@@ -3,13 +3,13 @@ __author__ = 'Tony'
 import os
 
 filepath = 'D:/source/Testing/modules/os/path.py'
-cwd = os.getcwd()
-basename = os.path.basename(filepath)
-abspath = os.path.abspath('../..')
-print 'cwd= ' + cwd
-print 'basename= ' + basename
-print 'abspath = ' + abspath
+
+print 'cwd= ' + os.getcwd()
+print 'listdir = ' , os.listdir('D:/source/Testing')
+print 'isfile = ' + str(os.path.isfile(filepath))
+print 'isdir = ' + str(os.path.isdir(filepath))
+print 'basename= ' + os.path.basename(filepath)
+print 'abspath = ' + os.path.abspath('../..')
 print 'split = ' , os.path.split(filepath)
 print 'dirname = ', os.path.dirname(filepath)
 print 'commonprefix = ', os.path.commonprefix(['/home/td','/home/td/ff','/home/td/fff'])
-
