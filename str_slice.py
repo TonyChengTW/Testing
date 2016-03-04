247a100f32bb0323fe2cc5521eea1609026b099b
TonyCheng <tony.pig@gmail.com>
2016-03-04 22:59:25 +0800

    commit xxxx

--- str_slice.py
+++ str_slice.py
@@ -17,3 +17,6 @@ if __name__ == '__main__':
     a2 = 'tony'
     a3 = ['item1','item2','item3']
     print 'a1=%d, a2=%s, a3=%s' % (a1, a2, a3)
+
+    dict = {'d1': 10, 'd2': 20 , 'd3': 30}
+    print 'dict = %(d1)s , %(d2)d , %(d3)s '% dict
