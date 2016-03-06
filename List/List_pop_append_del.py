540dec8429a3134f00b8c1d36db0825d460a5280
TonyCheng <tony.pig@gmail.com>
2016-03-06 23:59:42 +0800

    add dict print format

--- List/List_pop_append_del.py
+++ List/List_pop_append_del.py
@@ -3,15 +3,18 @@ list = [1, 2, 3, 4, 5, 6]
 list.pop()
 print list
 print (list)
-print id(list)
+print "list (Mutable) object id (pop) = " + str(id(list)
 
 list.append(7)
 print list
-print id(list)
+print "list (Mutable) object id (append) = " + str(id(list)
 
 del list[2]
 print list
 print id(list)
 
 
-aaa =
+aaa = 123
+print "aaa (Immutable) object id (123) = " + str(id(aaa))
+aaa = 456
+print "aaa (Immutable) object id (456) = " + str(id(aaa))
