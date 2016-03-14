9fa2363aead3a09de0a20bf0e9194e852d14b37f
Tony Cheng <tony.pig@gmail.com>
2016-03-14 10:54:14 +0800

    refactor execising dir name

--- modules/PasteDeploy/exercise1/master_valve.py
+++ modules/PasteDeploy/exercise1/master_valve.py
@@ -1,4 +1,6 @@
 if __name__ == '__main__':
     from paste import httpserver
     from paste.deploy import loadapp
-    httpserver.serve
+
+    httpserver.serve(loadapp('configured.ini', relative_to = ''),
+                     host = '0.0.0.0', port = '8000')
