d6c8bb3dac59acba311ad93ea82352153ac50c95
Tony Cheng <tony.pig@gmail.com>
2016-03-15 14:36:26 +0800

    refactor with another dir

--- T_modules/T_PasteDeploy/exercise1/master_valve.py
+++ T_modules/T_PasteDeploy/exercise1/master_valve.py
@@ -2,5 +2,5 @@ if __name__ == '__main__':
     from paste import httpserver
     from paste.deploy import loadapp
 
-    httpserver.serve(loadapp('configured.ini', relative_to = ''),
+    httpserver.serve(loadapp('config:configured.ini', relative_to = '.'),
                      host = '0.0.0.0', port = '8000')
