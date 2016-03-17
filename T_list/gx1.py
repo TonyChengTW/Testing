ff861deb6c6221ca26668377e3f1efc2207d84fd
TonyCheng <tony.pig@gmail.com>
2016-03-18 01:13:06 +0800

    comprehansion

---
+++ T_list/gx1.py
@@ -0,0 +1,28 @@
+classmates = ['Michael', 'Bob', 'Tracy']
+classmates.insert(1,'Jack')
+print classmates
+
+classmates = ('Michael', 'Bob', 'Tracy')
+classmates = ('Jack',)
+print type(classmates)
+print classmates
+
+s = set([1, 2, 3])
+s.add(4)
+print s
+
+s.remove(2)
+print s
+
+l = range(100)
+print l
+
+print l[1:10]
+print l[:10]
+print l[3:]
+print l[-10:]
+print l[:20:2]
+
+print 'comprehasion:\n'
+a = [x*2 for x in l]
+print a
