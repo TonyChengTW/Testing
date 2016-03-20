48175e662de04a0dce584e3ecd278b9e923de688
TonyCheng <tony.pig@gmail.com>
2016-03-20 11:21:53 +0800

    1st comprehansion

--- T_statment/comprehansion.py
+++ T_statment/comprehansion.py
@@ -1,3 +1,13 @@
 __author__ = 'Tony'
 a = [x*x for x in range(1,10) if x != 5]
+print type(a)
 print a
+
+b = (x*x for x in range(1,10) if x != 5)
+print type(b)
+
+print b.next()
+print b.next()
+print b.next()
+print b.next()
+print b.next()
