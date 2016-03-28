4986b7f9fef4f82488f60594f87c891d302e0531
TonyCheng <tony.pig@gmail.com>
2016-03-28 21:13:39 +0800

    random practice

---
+++ T_modules/random/random1.py
@@ -0,0 +1,11 @@
+__author__ = 'Tony'
+import random
+
+a = random.randint(10,20)
+print a
+
+b = random.choice('1qaz2wsx')
+print b
+
+c = random.choice ( ['apple', 'pear', 'peach', 'orange', 'lemon'] )
+print c
